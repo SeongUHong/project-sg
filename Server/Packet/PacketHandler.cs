44e2ba1f817d6d1ab01b51c4e3d449ecd08bd741
SeongUHong <swhong369@gmail.com>
2024-03-11 22:02:39 +0900

    updated packet system

---
+++ Server/Packet/PacketHandler.cs
@@ -0,0 +1,10 @@
+﻿using ServerCore;
+using System;
+
+class PacketHandler
+{
+    internal static void C_LeaveGameHandler(PacketSession arg1, IPacket arg2)
+    {
+        throw new NotImplementedException();
+    }
+}
