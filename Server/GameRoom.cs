8322960f751a1266203f0fe076b2cf26b701c783
SeongUHong <swhong369@gmail.com>
2024-03-14 22:03:57 +0900

    JobQqueue and GameRoom

---
+++ Server/GameRoom.cs
@@ -0,0 +1,51 @@
+﻿using ServerCore;
+using System;
+using System.Collections.Generic;
+
+namespace Server
+{
+    class GameRoom : IJobQueue
+    {
+        List<ClientSession> _sessions = new List<ClientSession>();
+        JobQueue _jobQueue = new JobQueue();
+        List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
+
+        public void Push(Action job)
+        {
+            _jobQueue.Push(job);
+        }
+
+        // 대기중인 작업들을 진행시킴
+        public void Flush()
+        {
+            foreach (ClientSession s in _sessions)
+                s.Send(_pendingList);
+
+            _pendingList.Clear();
+        }
+
+        // 정보 전송
+        public void Broadcast(ArraySegment<byte> segment)
+        {
+            _pendingList.Add(segment);
+        }
+
+        // 플레이어 입장
+        public void Enter(ClientSession session)
+        {
+
+        }
+
+        // 플레이어 퇴장
+        public void Leave(ClientSession session)
+        {
+
+        }
+
+        // 플레이어 이동
+        public void Move(ClientSession session)
+        {
+
+        }
+    }
+}
---
+++ ServerCore/JobQueue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerCore
+{
+    public class JobQueue : IJobQueue
+    {
+        Queue<Action> _jobQueue = new Queue<Action>();
+        object _lock = new object();
+        bool _flush = false;
+
+        // 작업들을 순서대로 대기시킴
+        // 작업 진행이 가능해졌다면 Flush()를 호출함
+        public void Push(Action job)
+        {
+            bool flush = false;
+
+            lock (_lock)
+            {
+                _jobQueue.Enqueue(job);
+                if (_flush == false)
+                    flush = _flush = true;
+            }
+
+            if (flush)
+                Flush();
+        }
+
+
+        // 대기중인 작업들을 진행시킴
+        void Flush()
+        {
+            while (true)
+            {
+                Action action = Pop();
+                if (action == null)
+                    return;
+
+                action.Invoke();
+            }
+        }
+
+        Action Pop()
+        {
+            lock (_lock)
+            {
+                if (_jobQueue.Count == 0)
+                {
+                    _flush = false;
+                    return null;
+                }
+                return _jobQueue.Dequeue();
+            }
+        }
+    }
+}
